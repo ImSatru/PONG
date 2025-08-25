a69732fd498fac4fa59f19651fda49ab260a6953
Satru <130175521+ImSatru@users.noreply.github.com>
2025-08-25 20:05:00 +0000

    add: Users schemas added

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { GameModule } from './game/game.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [UsersModule, GameModule],
+  imports: [UsersModule, GameModule,],
   controllers: [AppController],
   providers: [AppService],
 })
